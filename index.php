92bafc8a5c4b530844aa6217d049ff2cff740382
SergioComeron <sergiocomeron@icloud.com>
2026-07-01 10:33:53 +0200

    Fix: resolver config.php sin resolver symlinks en endpoints web
    Con el plugin symlinkeado en desarrollo, __DIR__/__FILE__ y '..' resuelven el
    symlink y config.php cae fuera del árbol de Moodle. Se usa
    dirname($_SERVER['SCRIPT_FILENAME'], 3) . '/config.php' (recorte lexicográfico),
    que funciona tanto symlinkeado como con ficheros físicos en producción.

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once(__DIR__ . '/../../config.php');
+require_once(dirname($_SERVER['SCRIPT_FILENAME'], 3) . '/config.php');
 require_once($CFG->dirroot . '/report/courseradar/locallib.php');
 
 // Parameters.
